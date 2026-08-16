2e195acd460179447529795a630b5e4786ead477
islandcontroller <13700483+islandcontroller@users.noreply.github.com>
2026-08-16 20:28:45 +0000

    Fixed assert function

--- Template/stm32g4xx_hal_conf_template.h
+++ Template/stm32g4xx_hal_conf_template.h
@@ -354,22 +354,8 @@ The real value may vary depending on the variations in voltage and temperature.*
 #include "stm32g4xx_hal_wwdg.h"
 #endif /* HAL_WWDG_MODULE_ENABLED */
 
-/* Exported macro ------------------------------------------------------------*/
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  The assert_param macro is used for function's parameters check.
-  * @param  expr: If expr is false, it calls assert_failed function
-  *         which reports the name of the source file and the source
-  *         line number of the call that failed.
-  *         If expr is true, it returns no value.
-  * @retval None
-  */
-#define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
-/* Exported functions ------------------------------------------------------- */
-void assert_failed(uint8_t *file, uint32_t line);
-#else
-#define assert_param(expr) ((void)0U)
-#endif /* USE_FULL_ASSERT */
+/*- Assert macro redirection -------------------------------------------------*/
+#include "stm32g4xx_ll_assert.h"
 
 #ifdef __cplusplus
 }
